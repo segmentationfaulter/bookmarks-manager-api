9ebc697a60c1616b8176642f91c578d86845877a
Muhammad Saqib <muhammadsackib@gmail.com>
2025-07-27 11:29:17 +0500

    read request body

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,14 +11,18 @@ func main() {
 	log.Fatal(http.ListenAndServe(":3000", Mux()))
 }
 
+type RegisterationData struct {
+	Username string
+	Email    string
+	Password string
+}
+
 func Mux() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		header := w.Header()
-		header.Add("content-type", "application/json")
-		json.NewEncoder(w).Encode(struct {
-			Message string `json:"message"`
-		}{Message: "Registeration successful"})
+	mux.HandleFunc("POST /register", func(w http.ResponseWriter, r *http.Request) {
+		var regData = RegisterationData{}
+		json.NewDecoder(r.Body).Decode(&regData)
+		fmt.Printf("username: %v\nemail: %v\n", regData.Username, regData.Email)
 	})
 
 	return mux
